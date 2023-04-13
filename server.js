6d00594bfa4fa2167a44864f34661141c7c104b0
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-04-13 13:01:20 +0300

    create mongoose connect

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+// AppYuz9qtfkS4l7N;
+const mongoose = require("mongoose");
 require("dotenv").config();
-const app = require("./app");
 
+const app = require("./app");
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+const DB_HOST =
+  "mongodb+srv://GoDoc:AppYuz9qtfkS4l7N@cluster0.kmqezx3.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    })
+  )
+  .catch((error) => console.log(error.message));
+
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`);
+// });
--- utils/validateBody.js
+++ utils/validateBody.js
@@ -1,16 +1,3 @@
-// const { HttpError } = require("../helpers");
-
-// const validateBody = (schema) => {
-//   const func = async (req, res, next) => {
-//     const { error } = schema.validate(req.body);
-//     if (error) {
-//       next(HttpError(400, error.message));
-//     }
-//     next();
-//   };
-//   return func;
-// };
-
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     if (Object.keys(req.body).length === 0) {
