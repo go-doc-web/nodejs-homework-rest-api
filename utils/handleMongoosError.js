64a208cec2d89d6831828835c9571e2de14502b0
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-04-30 14:13:20 +0300

    end hw-4

--- utils/handleMongoosError.js
+++ utils/handleMongoosError.js
@@ -1,5 +1,10 @@
 const handleMongooseError = (error, data, next) => {
-  error.status = 400;
+  console.log(error.name);
+  console.log(error.code);
+
+  const { name, code } = error;
+
+  error.status = name === 'MongoServerError' && code === 11000 ? 409 : 400;
   next();
 };
 
